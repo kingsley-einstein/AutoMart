43d81acaa3ae6886eed4cb6a768b7a627e538cc8
Kingsley Victor <javaprodigy56@gmail.com>
2019-06-02 04:06:48 -0700

    chore: removed auth folder.

--- Server/models/user.js
+++ Server/models/user.js
@@ -1,6 +1,6 @@
 import { hashSync, genSaltSync } from 'bcryptjs';
 import jwt from 'jsonwebtoken';
-import { authObj } from '../auth/passport';
+import { authObj } from '../auth';
 
 const { options } = authObj;
 
