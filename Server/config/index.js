9f3b7fd1203fb7c074138064f60ef39078e69e55
Kingsley Victor <javaprodigy56@gmail.com>
2019-06-03 16:54:33 -0700

    fix: commented out irrelevant snippet

--- Server/config/index.js
+++ Server/config/index.js
@@ -1,5 +1,5 @@
 import { json, urlencoded } from 'body-parser';
-import logger from 'morgan';
+// import logger from 'morgan';
 import router from '../routes';
 // import { authObj } from '../auth/passport';
 
@@ -10,7 +10,7 @@ export default class Config {
     // app.use(passport.initialize());
     app.use(json());
     app.use(urlencoded({ extended: true }));
-    app.use(logger('dev'));
+    // app.use(logger('dev'));
     app.use('/api/v1', router);
   }
 }
