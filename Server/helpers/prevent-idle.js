31e5515de50a6c3763d87b863dc07b5ef1e040f1
Kingsley Victor <javaprodigy56@gmail.com>
2019-06-04 23:20:12 -0700

    chore: updated prevent-idle.js

--- Server/helpers/prevent-idle.js
+++ Server/helpers/prevent-idle.js
@@ -3,7 +3,7 @@ import http from 'http';
 export const preventIdle = () => {
   setInterval(() => {
     http.get({
-      host: 'https://automart-andela.herokuapp.com',
+      host: 'http://automart-andela.herokuapp.com',
       port: process.env.PORT,
       path: '/api/v1'
     }, (res) => {
