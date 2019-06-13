0c7f98e8bab06de93064c270be391c98ca062e35
Kingsley Victor <javaprodigy56@gmail.com>
2019-06-13 10:38:26 -0700

    chore: configured db

---
+++ Server/db/config/index.js
@@ -0,0 +1,36 @@
+import { Pool } from 'pg';
+import { environment } from '../../environment';
+
+const {
+  database, user, password, port
+} = environment;
+
+export const pool = new Pool({
+  user,
+  database,
+  password,
+  port,
+  max: 10,
+  idleTimeoutMillis: 30000
+});
+
+pool.on('connect', () => {
+  console.log('Connected to db');
+});
+
+/**
+ *
+ * @param {any[]} queries
+ */
+export const createTables = (queries) => {
+  queries.forEach((query) => {
+    pool.query(query).then((res) => {
+      console.log(res);
+      pool.end();
+    })
+      .catch((err) => {
+        console.log(err);
+        pool.end();
+      });
+  });
+};
