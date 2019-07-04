d38f63abd3794c2bd00d530cbb9eee55319e8dd2
Kingsley Victor <javaprodigy56@gmail.com>
2019-07-04 10:43:05 -0700

    chore: set app to allow all origins

--- Server/config/index.js
+++ Server/config/index.js
@@ -9,6 +9,11 @@ import router from '../routes';
 export default class Config {
   configure(app) {
     // app.use(passport.initialize());
+    app.use((req, res, next) => {
+      res.header('Access-Control-Allow-Origin', '*');
+      res.header('Access-Control-Allow-Methods', '*');
+      next();
+    });
     app.use(json());
     app.use(urlencoded({ extended: true }));
     // app.use(logger('dev'));
