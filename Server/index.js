9d913a0e0f528e99f10cee503f9fcea0c3c63efd
Kingsley Victor <javaprodigy56@gmail.com>
2019-07-15 09:47:38 -0700

    chore: update docs

--- Server/index.js
+++ Server/index.js
@@ -10,7 +10,7 @@ config.configure(app);
 
 const create = [
   'CREATE TABLE IF NOT EXISTS users(id SERIAL PRIMARY KEY, first_name VARCHAR (50), last_name VARCHAR (50), password VARCHAR (80) NOT NULL, address VARCHAR (250) NOT NULL, is_admin boolean, email VARCHAR (250), token VARCHAR (250))',
-  'CREATE TABLE IF NOT EXISTS cars(id SERIAL PRIMARY KEY, created_on TIMESTAMP, state VARCHAR (20), price FLOAT, manufacturer VARCHAR (20), model VARCHAR (20), body_type VARCHAR (20), img_url VARCHAR (20))',
+  'CREATE TABLE IF NOT EXISTS cars(id SERIAL PRIMARY KEY, created_on TIMESTAMP, state VARCHAR (20), price FLOAT, manufacturer VARCHAR (20), model VARCHAR (20), body_type VARCHAR (20), img_url VARCHAR (20), status VARCHAR (20))',
   'CREATE TABLE IF NOT EXISTS orders(id SERIAL PRIMARY KEY, amount FLOAT, status VARCHAR (20))',
   'CREATE TABLE IF NOT EXISTS flags(id SERIAL PRIMARY KEY, created_on TIMESTAMP, reason VARCHAR (255), description VARCHAR (255))'
 ];
