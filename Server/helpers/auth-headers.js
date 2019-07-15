8d391841b58bc5b90542b2b58e49d63be5cff2b9
Kingsley Victor <javaprodigy56@gmail.com>
2019-07-15 10:59:55 -0700

    chore: update auth file

--- Server/helpers/auth-headers.js
+++ Server/helpers/auth-headers.js
@@ -3,19 +3,19 @@ import { pool } from '../db/config';
 
 export class TokenExtractor {
   async extractTokenFromHeader(req, res, next) {
-    const { authorization } = req.headers;
+    // const { authorization } = req.headers;
     const { token } = req.body;
-    if (!authorization && !token) {
+    if (!token) {
       res.status(401).json({
         status: 401,
-        error: 'Auth header or token is null'
+        error: 'Token is null'
       });
       return;
     }
     const user = await new Promise((resolve, reject) => {
       pool
         .query('SELECT * FROM users WHERE token = $1', [
-          authorization.split(' ')[1] || token
+          token
         ])
         .then((data) => {
           const { rows } = data;
